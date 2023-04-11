1cb1cd9ec9cdb70d286c526df5e87c23b6da6175
Ronit Gurjar <ronitgjcarl0ver.com>
2023-04-11 21:28:57 +0530

    Adding a translate button

--- tran.py
+++ tran.py
@@ -9,9 +9,12 @@ root = Tk()
 root.title("Language translator")
 root.geometry("880x300")
 
-#Text boxes
+#Text boxes & buttons
 original_text= Text(root, height=10, width=40)
 original_text.grid(row=0, column=0, pady=20,padx=10)
+
+translate_button= Button(root, text="Translate!", command=translate_it )
+translate_button.grid(row=0, column=1, padx=10)
 
 translated_text= Text(root, height=10, width=40)
 translated_text.grid(row=0, column=2, pady=20,padx=10)
