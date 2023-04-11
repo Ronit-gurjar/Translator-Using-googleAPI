567b04da4b58be18942cf8d71db3786863ac330e
Ronit Gurjar <ronitgjcarl0ver.com>
2023-04-11 21:19:29 +0530

    Adding imports

--- tran.py
+++ tran.py
@@ -1,6 +1,10 @@
 #imports
 from tkinter import *
+import googletrans
+import textblob
+from tkinter import ttk, messagebox
 
+#app:-
 root = Tk()
 root.title("Language translator")
 root.geometry("880x300")
