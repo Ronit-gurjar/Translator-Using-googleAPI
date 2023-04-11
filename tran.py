77a8b94d047fbec1a1542401fbfba64e74b25c18
Ronit Gurjar <ronitgjcarl0ver.com>
2023-04-11 21:45:58 +0530

    adding Combo boxes for language selection

--- tran.py
+++ tran.py
@@ -13,14 +13,27 @@ root.geometry("770x300")
 def translate_it():
     pass
 
-#Text boxes & buttons
+"""Text boxes, Combo boxes & buttons"""
+#Text box1
 original_text= Text(root, height=10, width=40)
 original_text.grid(row=0, column=0, pady=20,padx=10)
 
+#button1
 translate_button= Button(root, text="Translate!", command=translate_it )
 translate_button.grid(row=0, column=1, padx=10)
 
+#Text box2
 translated_text= Text(root, height=10, width=40)
 translated_text.grid(row=0, column=2, pady=20,padx=10)
+
+#Combo box1
+original_combo=ttk.Combobox(root, width=50, value=language_list)
+original_combo.current(21)
+original_combo.grid(row=1, column=0)
+
+#Combo box2
+translated_combo=ttk.Combobox(root, width=50, value=language_list)
+translated_combo.current(21)
+translated_combo.grid(row=1, column=0)
 
 root.mainloop()
