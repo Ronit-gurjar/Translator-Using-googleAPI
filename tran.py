c23fc22424042f19266b2cfabc1c4c5ea3005be2
Ronit Gurjar <ronitgjcarl0ver.com>
2023-04-11 21:24:18 +0530

    Adding some boxes for text

--- tran.py
+++ tran.py
@@ -9,4 +9,11 @@ root = Tk()
 root.title("Language translator")
 root.geometry("880x300")
 
+#Text boxes
+original_text= Text(root, height=10, width=40)
+original_text.grid(row=0, column=0, pady=20,padx=10)
+
+translated_text= Text(root, height=10, width=40)
+translated_text.grid(row=0, column=2, pady=20,padx=10)
+
 root.mainloop()
